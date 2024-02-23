dcbc3e19ad4b8db2026b0590d3ba436006cd2062
FoxTimew <foxtimew@hotmail.com>
2024-02-23 17:01:06 +0100

    optimisation
    format and size optimised

--- Javascript/FadeEffects.js
+++ Javascript/FadeEffects.js
@@ -1,6 +1,6 @@
 var titleImage = document.getElementById('SG-logo');
 
-window.addEventListener('scroll', function() {
+window.addEventListener('scroll', function () {
     var scrollPosition = window.scrollY;
 
     // Calculer la taille de l'image et l'opacité en fonction du défilement
@@ -8,17 +8,16 @@ window.addEventListener('scroll', function() {
     var opacity = 1;
 
     // Appliquer les styles à l'image
+    titleImage.style.position ='fixed';
     titleImage.style.transform = 'translate(-50%, -50%) scale(' + scale + ')';
     titleImage.style.opacity = 1;
-    if (scrollPosition > 700) {
-        opacity = 1 - (scrollPosition - 700) / 300;
-        titleImage.style.opacity = opacity;
+    if (scrollPosition > 1000) {
+        titleImage.style.position = 'absolute';
     }
     // Masquer l'image lorsque l'utilisateur a défilé au-delà de la hauteur de la page
-    else if (scrollPosition > 1000) {
-        titleImage.classList.add('hidden');
-    } else {
-        titleImage.classList.remove('hidden');
+    else if (scrollPosition > 700) {
+        opacity = 1 - (scrollPosition - 700) / 300;
+        titleImage.style.opacity = opacity;
     }
 });
 
--- Javascript/ImgScroll.js
+++ Javascript/ImgScroll.js
@@ -1,44 +1,36 @@
 var image = document.getElementById('img-scroll');
-var body = document.body;
 var isAnimationFinished = true;
 var endPosition = -1;
 
-window.addEventListener('scroll', function() {
+window.addEventListener('scroll', function () {
     var scrollPosition = Math.ceil(window.scrollY / 13);
+    document.getElementById('img-scroll-anim').style.height = 7358 + (window.innerHeight)+'px';
     if (isAnimationFinished) {
         changeImage(scrollPosition);
     }
     else if (window.scrollY <= endPosition){
         isAnimationFinished = true;
         image.style.position = 'fixed';
-        image.style.top = '50%';
-        image.style.left = '50%';
-        image.style.width = '100vw'
-        image.style.transform = 'translate(-50%, -50%)';
-        body.style.backgroundColor = '#f14ffd';
+        image.style.top = '0px';
     }
 });
 
 function changeImage(frame) {     
     // Changez les images en fonction de la position de défilement
     if (frame < 10) {
-        image.src = 'content/images/img_scroll_anim/000' + frame + '.png';
+        image.src = 'content/images/img_scroll_anim/000' + frame + '.avif';
     } else if (frame < 100) {
-        image.src = 'content/images/img_scroll_anim/00' + frame + '.png';
+        image.src = 'content/images/img_scroll_anim/00' + frame + '.avif';
     } else if (frame <= 566) {
-        image.src = 'content/images/img_scroll_anim/0' + frame + '.png';
+        image.src = 'content/images/img_scroll_anim/0' + frame + '.avif';
     } else {
-        image.src = 'content/images/img_scroll_anim/0566.png';
-        endPosition = window.scrollY;
-        image.style.position = 'absolute';
-        image.style.top = (window.scrollY + window.innerHeight / 2) + 'px';
-        image.style.left = '50%';
-        image.style.transform = 'translate(-50%, -50%)';
-        image.style.width = '100vw';
         isAnimationFinished = false;
-        body.style.backgroundColor = 'white';
+        image.src = 'content/images/img_scroll_anim/0566.avif';
+        image.style.position = 'absolute';
+        image.style.top = 7358 + 'px';
+        endPosition = window.scrollY;
+        console.log(endPosition);
     }
-
 }
 
 console.log("imgScroll loaded");
