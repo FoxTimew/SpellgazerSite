2253f22f9396b0f25e2057b0b86a29a290244f83
FoxTimew <foxtimew@hotmail.com>
2024-03-11 16:52:16 +0100

    update site

--- Javascript/YoutubeVideos.js
+++ Javascript/YoutubeVideos.js
@@ -1,8 +1,39 @@
 // YouTube video initialisation
-var player;
+var player0;
+var player1;
+var player2;
+
 function onYouTubeIframeAPIReady() {
-    player = new YT.Player('player', {
-        videoId: 'NpEaa2P7qZI',
+    player0 = new YT.Player('player0', {
+        videoId: 'dopBWKMDgIM',
+        playerVars: {
+            'autoplay': 1,
+            'controls': 1,
+            'rel': 0,
+            'showinfo': 0,
+            'playsinline': 1
+        },
+        events: {
+            'onReady': onPlayerReady
+        }
+    });
+
+    player1 = new YT.Player('player1', {
+        videoId: 'NNzEPjX_wH4',
+        playerVars: {
+            'autoplay': 1,
+            'controls': 1,
+            'rel': 0,
+            'showinfo': 0,
+            'playsinline': 1
+        },
+        events: {
+            'onReady': onPlayerReady
+        }
+    });
+
+    player2 = new YT.Player('player2', {
+        videoId: 'Fn0hJQ8CSfE',
         playerVars: {
             'autoplay': 1,
             'controls': 1,
